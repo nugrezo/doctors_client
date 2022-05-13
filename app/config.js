e20b022e01da497f16326b2720b5222c7932ff2b
Ergun Oz <ergun.oz82@gmail.com>
2022-05-12 23:23:55 -0400

    api has been changed in main branch

--- app/config.js
+++ app/config.js
@@ -1,7 +1,7 @@
 let apiUrl
 //added heroku url
 const apiUrls = {
-  production: 'https://polar-waters-35134.herokuapp.com',
+  production: 'https://shrouded-forest-65579.herokuapp.com',
   development: 'http://127.0.0.1:4741'
 }
 
